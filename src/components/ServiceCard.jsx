a56a83f23326e24992b1affeef2c1a7badacf03b
andrea gronchi <andreagronchi21@gmail.com>
2023-10-27 00:29:14 +0200

    still trying

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,31 +1,59 @@
+
 import React, { useState } from 'react';
 
+export default function ServiceCard({ title, textDescription }) {
+  const isDesktop = window.innerWidth > 840;
+  const [isDescriptionOpen, setIsDescriptionOpen] = useState(false);
+
+  const toggleDescription = () => {
+    setIsDescriptionOpen(!isDescriptionOpen);
+  }
+
+  return (
+    <article className="service-card_container">
+      {!isDesktop ? (
+        <button onClick={toggleDescription}>
+          <h3>{title}</h3>
+        </button>
+      ) : (
+        <h3 onClick={toggleDescription}>{title}</h3>
+      )}
+      {isDescriptionOpen && (
+        <p className='textDesc'>{textDescription}</p>
+      )}
+    </article>
+  );
+}
+
+
+
+
+/* import React, { useState } from 'react';
+
 function ServiceCard({ title, textDescription }) {
-    const [isDesktop, setisDesktop] = useState(true);
+    const isDesktop = window.innerWidth > 840; //true
+    console.log(isDesktop, "desktop")
+    const [showDesc, setshowDesc] = useState(false);
+    console.log(showDesc, "text")
+
 
     const handleTitleClick = () => {
-            setisDesktop(!isDesktop); 
-            if (!isDesktop) {
-                const allTextDescElements = document.querySelectorAll('.textDesc');
-                allTextDescElements.forEach((element) => {
-                    element.style.display = 'none';
-                });
-            }else if(isDesktop) {
-                const allTextDescElements = document.querySelectorAll('.textDesc');
-                allTextDescElements.forEach((element) => {
-                    element.style.display = 'block';
-                });
-            }
-    };
-
+        if (!isDesktop) {
+            setshowDesc(!showDesc)
+        }
+    }
     return (
         <article className="service-card_container">
-            <button onClick={handleTitleClick} >
-            <h3 >{title}</h3>
-            </button>
-            { isDesktop ? <p className='textDesc'>{textDescription}</p> : <p className='notvisible'></p>}
+            {!isDesktop ?
+                <button onClick={handleTitleClick}>
+                    <h3>{title}</h3>
+                </button> : <h3 >{title}</h3>}
+            {isDesktop || showDesc ?
+                <p className='textDesc'>{textDescription}</p> :
+                <p className='notvisible'></p>}
         </article>
     );
 }
+export default ServiceCard; */
 
-export default ServiceCard;
+
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,23 +1,29 @@
+import React from 'react';
 import ServiceCard from "../components/ServiceCard"
 import TekSkills from "../components/Tekskills";
+
 function Services() {
+
   const titleWebDev = `I can design and develop bespoke software to address your specific requirements, whether it's a web, mobile, or desktop application. I'm here to transform your ideas into high-quality code.`;
   const titlePerf =
     "If you have an existing application that's sluggish or inefficient, I can optimize it to enhance performance and user satisfaction. Every millisecond counts, and swift loading is crucial.";
   const titleSec = "Data security is paramount. I can assist you in safeguarding sensitive data and implementing top-notch cybersecurity practices to prevent breaches. "
   const titleGreen = `"I don't just stop after the project is done. I provide ongoing tech support, maintenance, and updates to keep your software up to date. Plus, I'm all about 'green code' practices, making sure your software runs efficiently and is environmentally friendly."`;
+  
+  
   return (
     <main className="main-service">
       <h1 id="my-service">What I Can Do For You:</h1>
-      {/*   <p>Technology has the power to transform your business and enhance efficiency in many ways. I'm here to guide you on this journey and ensure you gain the utmost value from your software development investment.</p> */}
       <div className="service-container">
         <ServiceCard
           title="Custom Software Development"
           textDescription={titleWebDev}
+         
         />
         <ServiceCard
           title="Performance Optimization"
           textDescription={titlePerf}
+        
         />
         <ServiceCard
           title=" Technical Support & Green Code"
